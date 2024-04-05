79440ca4b9c92f4a2f794e5e6630745ee0201581
Andres Rojas <afrojasca@ufpso.edu.co>
2024-04-05 10:01:50 -0500

    hola

---
+++ intento1/src/main/java/com/mycompany/intento1/Intento1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.intento1;
+
+/**
+ *
+ * @author SCIS2-22
+ */
+public class Intento1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
